728230bd9fda564605bdc39e8cd064ac1cef48fb
Daniel <danieljimgarcia@gmail.com>
2015-10-21 22:43:53 +0100

    Add Manual workflow sample project

---
+++ ManualWorkflow/src/foo.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="./bar.ts" />
+
+module app {
+
+    export class Foo {
+        constructor(private bar: IBar) {
+        }
+
+        public doWork(data: string) {
+            this.bar.doAdditionalWork(data);
+        }
+    }
+
+}
---
+++ ManualWorkflow/test/fooTest.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="../src/foo.ts" />
+/// <reference path="./testCommon.ts" />
+
+var sandbox: SinonSandbox,
+    bar: app.IBar;
+
+QUnit.module("fooTests", {
+    setup: () => {
+        sandbox = sinon.sandbox.create();
+
+        bar = {
+            doAdditionalWork: (data: string) => { }
+        };
+    },
+    teardown: () => {
+        sandbox.restore();
+    }
+});
+
+test("dummy test", () => {
+    var foo = new app.Foo(bar);
+    ok(foo != null, "foo was initialized");
+});
+
+test("dummy collaboration test", () => {
+    var doAdditionalWorkSpy = sinon.spy(bar, "doAdditionalWork");
+
+    var foo = new app.Foo(bar);
+    foo.doWork("dummyData");
+
+    sinon.assert.calledWith(doAdditionalWorkSpy, "dummyData");
+});
